d8b2bbc0b1f1cacbf2e71508d1270f4244f54a3f
Harsh Devisha <harshdevisha9111@gmail.com>
2024-10-19 20:00:41 -0700

    (feat) format on save

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -11,3 +11,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	pattern = "*",
 })
 
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWriteLock",
+})
