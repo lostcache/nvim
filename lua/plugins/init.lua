0cb93400b81ab3cea102a6957d8fe19ea8bfc5c7
lostcache <harshdevisha9111@gmail.com>
2026-09-11 14:19:45 -0700

    adds fugitive

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -48,6 +48,9 @@ vim.pack.add({
 	{ src = gh("nvim-pack/nvim-spectre"), version = "72f56f7585903cd7bf92c665351aa585e150af0f" },
 	{ src = gh("tribela/transparent.nvim"), version = "main" },
 
+	-- git
+	{ src = gh("tpope/vim-fugitive") },
+
 	-- Themes
 	{ src = gh("sainnhe/everforest") },
 	{ src = gh("folke/tokyonight.nvim") },
