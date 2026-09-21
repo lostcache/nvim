31067c6afd780b4a1e304b7445c939386d7447ee
lostcache <harshdevisha9111@gmail.com>
2026-09-21 11:02:47 -0700

    focus and pin trouble symbols

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -23,7 +23,7 @@ local map = vim.keymap.set
 map("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
 map("n", "<leader>td", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Diagnostics (Trouble)" })
 map("n", "<leader>tbd", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", { desc = "Buffer Diagnostics (Trouble)" })
-map("n", "<leader>ts", "<cmd>Trouble symbols toggle focus=false<cr>", { desc = "Symbols (Trouble)" })
+map("n", "<leader>ts", "<cmd>Trouble symbols toggle pinned=true focus=true<cr>", { desc = "Symbols (Trouble)" })
 map("n", "<leader>cl", "<cmd>Trouble lsp toggle focus=false win.position=right<cr>", { desc = "LSP Definitions / references / ... (Trouble)" })
 map("n", "<leader>xL", "<cmd>Trouble loclist toggle<cr>", { desc = "Location List (Trouble)" })
 map("n", "<leader>xQ", "<cmd>Trouble qflist toggle<cr>", { desc = "Quickfix List (Trouble)" })
